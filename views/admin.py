29e4e7788f88c4047f976d546a22bcc6f7463a59
Zhou-ly <1959649788@qq.com>
2020-12-08 17:07:55 +0800

    add login_required wrapper

--- views/admin.py
+++ views/admin.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import jwt
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -5,6 +7,24 @@ from app import app
 from flask import jsonify, request
 from models import User
 
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if "Authorization" in request.headers:
+            # Check whether token was sent
+            authorization_header = request.headers["Authorization"]
+
+            # Check whether token is valid
+            try:
+                token = authorization_header.split(" ")[1]
+                user = jwt.decode(token, app.config["SECRET_KEY"])
+            except:
+                return jsonify({"error": "you are not logged in"}), 401
+
+            return f(userid=user["userid"], *args, **kwargs)
+        else:
+            return jsonify({"error": "you are not logged in"}), 401
+    return wrap
 
 @app.route("/mp/v1_0/authorizations", methods=["POST"])
 def login():
@@ -58,9 +78,11 @@ def login():
     })
 
 @app.route("/mp/v1_0/user/profile", methods=["GET"])
-def get_user_profile():
-    user = User.objects(name='zhangsan').first()
+@login_required
+def get_user_profile(userid):
+    user = User.objects(id = userid).first()
     return jsonify({
         "message": 'OK',
         "data": user.to_public_json()
     })
+
